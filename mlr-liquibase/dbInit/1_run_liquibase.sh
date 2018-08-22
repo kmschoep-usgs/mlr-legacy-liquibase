fc9dc30fa363f85cad4976110919a8751eaf393f
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-08-22 10:54:59 -0500

    removed variables/parameters.

--- mlr-liquibase/dbInit/1_run_liquibase.sh
+++ mlr-liquibase/dbInit/1_run_liquibase.sh
@@ -5,13 +5,9 @@ LIQUIBASE_WORKSPACE=$2
 LIQUIBASE_VERSION=$3
 JDBC_JAR=$4
 POSTGRES_PASSWORD=$5
-MLR_LEGACY=$6
-MLR_LEGACY_PASSWORD=$7
-MLR_LEGACY_USER=$8
-MLR_LEGACY_USER_PASSWORD=$9
-MLR_LEGACY_DATA=${10}
-MLR_ADDRESS=${11}
-MLR_PORT=${12}
+MLR_LEGACY_PASSWORD=$6
+MLR_LEGACY_USER_PASSWORD=$7
+MLR_ADDRESS=$8
 
 # Restart postgres to make sure we can connect
 if [ -n "${LOCALONLY}" ]; then 
@@ -24,16 +20,13 @@ ${LIQUIBASE_HOME}/liquibase \
 	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
 	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/postgres/changeLog.yml \
 	--driver=org.postgresql.Driver \
-	--url=jdbc:postgresql://${MLR_ADDRESS}:${MLR_PORT}/postgres \
+	--url=jdbc:postgresql://${MLR_ADDRESS}:5432/postgres \
 	--username=postgres \
 	--password=${POSTGRES_PASSWORD} \
 	--logLevel=debug \
 	update \
 	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-	-DMLR_LEGACY_DATABASE_NAME=${MLR_LEGACY} \
-	-DMLR_LEGACY=${MLR_LEGACY} \
 	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
-	-DMLR_LEGACY_USER=${MLR_LEGACY_USER} \
 	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
 
 # postgres to mlr db scripts
@@ -41,30 +34,23 @@ ${LIQUIBASE_HOME}/liquibase \
 	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
 	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/mlr_legacy/changeLog.yml \
 	--driver=org.postgresql.Driver \
-	--url=jdbc:postgresql://${MLR_ADDRESS}:${MLR_PORT}/${MLR_LEGACY} \
+	--url=jdbc:postgresql://${MLR_ADDRESS}:5432/mlr_legacy \
 	--username=postgres \
 	--password=${POSTGRES_PASSWORD} \
 	--logLevel=debug \
 	update \
-	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-	-DMLR_LEGACY_DATABASE_NAME=${MLR_LEGACY} \
-	-DMLR_LEGACY_USER=${MLR_LEGACY_USER} \
-	-DMLR_LEGACY_SCHEMA_NAME=${MLR_LEGACY_DATA} 
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} 
 
 # remaining application scripts
 ${LIQUIBASE_HOME}/liquibase \
 	--defaultsFile=${LIQUIBASE_HOME}/liquibase.properties \
 	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
-	--changeLogFile=${LIQUIBASE_WORKSPACE}/${MLR_LEGACY}/changeLog.yml \
+	--changeLogFile=${LIQUIBASE_WORKSPACE}/mlr_legacy/changeLog.yml \
 	--driver=org.postgresql.Driver \
-	--url=jdbc:postgresql://${MLR_ADDRESS}:${MLR_PORT}/${MLR_LEGACY} \
-	--username=${MLR_LEGACY} \
+	--url=jdbc:postgresql://${MLR_ADDRESS}:5432/mlr_legacy \
+	--username=mlr_legacy \
 	--password=${MLR_LEGACY_PASSWORD} \
 	--logLevel=debug \
 	update \
-	-DMLR_LEGACY=${MLR_LEGACY} \
 	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
-	-DMLR_LEGACY_USER=${MLR_LEGACY_USER} \
-	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} \
-	-DMLR_LEGACY_DATABASE_NAME=${MLR_LEGACY} \
-	-DMLR_LEGACY_SCHEMA_NAME=${MLR_LEGACY_DATA} 
+	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD}
